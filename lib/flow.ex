1f99ef3290aee7bf4a979d90cec32ee3b112c1b1
yuriibodarev <yuriibodarev@gmail.com>
2016-10-31 22:50:38 +0200

    Function composition for Eager and Lazy processing

--- lib/flow.ex
+++ lib/flow.ex
@@ -5,23 +5,28 @@ defmodule Flow do
     path_to_file
     |> File.read!()
     |> String.split()
-    |> Enum.map(&String.replace(&1, ~r/\W/u, ""))
-    |> Enum.filter_map(fn s -> s != "" end, &String.downcase/1)
-    |> Enum.reduce(%{}, fn word, map ->
-        Map.update(map, word, 1, &(&1 + 1))
-      end)
+    |> Enum.reduce(%{}, &words_to_map/2)
   end
 
   def process_lazy(path_to_file) do
     path_to_file
     |> File.stream!()
     |> Stream.flat_map(&String.split/1)
-    |> Stream.map(&String.replace(&1, ~r/\W/u, ""))
-    |> Stream.filter_map(fn s -> s != "" end, &String.downcase/1)
-    |> Enum.reduce(%{}, fn word, map ->
-        Map.update(map, word, 1, &(&1 + 1))
-      end)
+    |> Enum.reduce(%{}, &words_to_map/2)
   end
+
+  defp words_to_map(word, map) do
+    word
+    |> String.replace(~r/\W/u, "")
+    |> filter_map(map)
+  end
+
+  defp filter_map("", map), do: map
+  defp filter_map(word, map) do
+    word = String.downcase(word)
+    Map.update(map, word, 1, &(&1 + 1))
+  end
+
 
   def process_flow(path_to_file) do
     path_to_file
@@ -29,11 +34,11 @@ defmodule Flow do
     |> Flow.from_enumerable()
     |> Flow.flat_map(&String.split/1)
     |> Flow.map(&String.replace(&1, ~r/\W/u, ""))
-    |> Flow.filter_map(fn s -> s != "" end, &String.downcase/1)
+    |> Flow.filter_map(fn w -> w != "" end, &String.downcase/1)
     |> Flow.partition()
     |> Flow.reduce(fn -> %{} end, fn word, map ->
-      Map.update(map, word, 1, &(&1 + 1))
-    end)
+        Map.update(map, word, 1, &(&1 + 1))
+       end)
     |> Enum.into(%{})
   end
 end
