feae7764567a58e403f897ac9604e5a8349ef0c1
yuriibodarev <yuriibodarev@gmail.com>
2016-11-04 18:41:35 +0200

    Timer functionality moved to separate module

--- try_me.exs
+++ try_me.exs
@@ -1,62 +1,24 @@
+alias Flow.Timer
+
 path_to_file = Path.absname("./files/medium.txt")
-
-parent = self
-
+path_to_dir = Path.absname("./files/parts_medium")
 
 IO.puts("Eager")
-spawn_link fn ->
-  send(parent, { :tc, :timer.tc(Flow, :process_eager, [path_to_file]) })
-end
-
-receive do
-  { :tc, {timer, map} } ->
-    IO.inspect timer / 1_000_000
-    IO.inspect map
-end
-
+Timer.run(Flow, :process_eager, [path_to_file]) |> IO.inspect()
 
 IO.puts("\nLazy")
-spawn_link fn ->
-  send(parent, { :tc, :timer.tc(Flow, :process_lazy, [path_to_file]) })
-end
-
-receive do
-  { :tc, {timer, map} } ->
-    IO.inspect timer / 1_000_000
-    IO.inspect map
-end
+Timer.run(Flow, :process_lazy, [path_to_file]) |> IO.inspect()
 
 
 IO.puts("\nFlow - single source")
-spawn_link fn ->
-  send(parent, { :tc, :timer.tc(Flow, :process_flow, [path_to_file]) })
-end
-
-receive do
-  { :tc, {timer, map} } ->
-    IO.inspect timer / 1_000_000
-    IO.inspect map
-end
-
-
-path_to_dir = Path.absname("./files/parts_medium")
+Timer.run(Flow, :process_flow, [path_to_file]) |> IO.inspect()
 
 IO.puts("\nFlow - multiple sources")
-spawn_link fn ->
-  send(parent, { :tc, :timer.tc(Flow, :process_flow_dir, [path_to_dir]) })
-end
-
-receive do
-  { :tc, {timer, map} } ->
-    IO.inspect timer / 1_000_000
-    IO.inspect map
-end
+Timer.run(Flow, :process_flow_dir, [path_to_dir]) |> IO.inspect()
 
 
 IO.puts("\nFlow - global window - count trigger - keep reducer accumulator")
-keep_events = Flow.window_global_trigger(1..1000, 100, :keep)
-IO.inspect(keep_events)
+Flow.window_global_trigger(1..1000, 100, :keep) |> IO.inspect()
 
 IO.puts("\nFlow - global window - count trigger - reset reducer accumulator")
-reset_reset = Flow.window_global_trigger(1..1000, 100, :reset)
-IO.inspect(reset_reset)
+Flow.window_global_trigger(1..1000, 100, :reset) |> IO.inspect()
